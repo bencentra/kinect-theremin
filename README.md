8b1ad2e6d032f59a3728d82d5e7cd73b696c7355
Ben Centra <bencentra@csh.rit.edu>
2013-02-27 23:03:23 -0500

    basic functionality complete

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Uses [WpfKinectHelper] (https://github.com/bencentra/WpfKinectHelper) for interf
 
 Made by [Ben Centra](https://github.com/bencentra). Based on an earlier project, [naudio-sinegen] (https://github.com/bencentra/naudio-sinegen).    
 
+How to Use
+----------
+
+1) Run the program and hit the "Play" button. The sine wave should begin to play.    
+2) Stand in front of your Kinect. Once your skeleton is detected, you can begin to make music!     
+3) Use your right hand to control the frequency of the sine wave (top of screen = higher pitch, bottom = lower).    
+4) Use your left hand to control the volume of the wave (top = louder, bottom = quieter).    
+5) You can change the "key" (the root note) using the dropdown menu on the left.    
+
+To-Do's / Future Improvements
+-----------------------------
+* Ability to change handedness (for lefties)    
+* Visual guides for control    
+* Discrete intervals using guides to hit exact notes    
--- kinect-theremin/MainWindow.xaml.cs
+++ kinect-theremin/MainWindow.xaml.cs
@@ -24,14 +24,138 @@ namespace kinect_theremin
     public partial class MainWindow : Window
     {
         // Object for playing the sine wave
-        private SineWavePlayer _player = new SineWavePlayer();
+        private SineWavePlayer _player;
 
         // Kinect helper
-        
+        private KinectHelper _helper;
 
+        // MainWindow Constructor 
         public MainWindow()
         {
             InitializeComponent();
+            // Instantiate the wave player
+            _player = new SineWavePlayer();
+            // Set up the UI
+            freqLabel.Content = _player.Frequency;
+            ampLabel.Content = _player.Amplitude;
+            for (int i = 0; i < _player.frequencyDict.Count; i++)
+            {
+                keyBox.Items.Add(_player.frequencyDict.ElementAt(i).Key);
+            }
+            keyBox.SelectedIndex = 0;
+            // Initialize the wave player
+            ChangeKey();
+            _player.Frequency = _player.MinFreq;
+            // Instantiate and initialize the KinectHelper
+            _helper = new KinectHelper(true, false, true);
+            _helper.ToggleSeatedMode(true);
+            _helper.SkeletonDataChanged += new KinectHelper.SkeletonDataChangedEvent(SkeletonDataChange);
+            skeletonImage.Source = _helper.skeletonBitmap;
+            rgbImage.Source = _helper.colorBitmap;
+        }
+
+        // Event handler for the playButton's Click event
+        // Used to start and stop playing the wave
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            // If the wave is not playing, start it
+            if (!_player.IsPlaying())
+                StartPlayer();
+            // If the wave is playing, stop it
+            else
+                StopPlayer();
+        }
+
+        // Event handler for the keyBox's SelectionChanged event
+        // Used to change the key of the wave
+        private void keyBox_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the current playing status of the wave
+            bool wasPlaying = false;
+            if (_player.IsPlaying())
+                wasPlaying = true;
+            // Stop the player
+            StopPlayer();
+            // Change the key
+            ChangeKey();
+            // If the wave was playing, start it again
+            Console.WriteLine(wasPlaying);
+            if (wasPlaying)
+                StartPlayer();
+        }
+
+        // Event handler for KinectHelper.SkeletonDataChanged event
+        // Used to get the positions of the user's hands and control the theremin
+        private void SkeletonDataChange(object o, SkeletonDataChangeEventArgs e)
+        {
+            // Get the primary skeleton (the first one being tracked)
+            Skeleton skel = null;
+            for (int i = 0; i < e.skeletons.Length; i++)
+            {
+                if (e.skeletons[i].TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    skel = e.skeletons[i];
+                    break;
+                }
+            }
+            // If no skeletons found, no need to continue
+            if (skel == null)
+                return;
+
+            // Get the left and right hand positions from the skeleton
+            Point rightHand = _helper.SkeletonPointToScreen(skel.Joints[JointType.HandRight].Position);
+            Point leftHand = _helper.SkeletonPointToScreen(skel.Joints[JointType.HandLeft].Position);
+
+            // Determine the frequency based on the position of the right hand
+            double freqValue = 1 - rightHand.Y / skeletonImage.Height;
+            float customFreq = LinearToLog(freqValue);  
+
+            // Determine the amplitude based on the position of the left hand
+            double ampValue = 1 - leftHand.Y / skeletonImage.Height;
+            float customAmp = (float) ampValue;
+            
+            // Update the wave
+            if (_player.IsPlaying())
+            {
+                _player.Frequency = customFreq;
+                freqLabel.Content = customFreq;
+                _player.Amplitude = customAmp;
+                ampLabel.Content = customAmp;
+            }
+        }
+
+        // Start the wave player
+        private void StartPlayer()
+        {
+            _player.Start();
+            playButton.Content = "Stop";
+        }
+
+        // Stop the wave player
+        private void StopPlayer()
+        {
+            _player.Stop();
+            playButton.Content = "Play";
+        }
+
+        // Change the key of the wave player
+        private void ChangeKey()
+        {
+            _player.ChangeKeyTo((String)keyBox.SelectedValue);
+            _player.Frequency = _player.MinFreq;
+            freqLabel.Content = _player.MinFreq;
+        }
+
+        // Convert a "linear" value (0 - 1) to a logarithmic frequency 
+        private float LinearToLog(double value)
+        {
+            return (float) Math.Pow(10, value * (Math.Log10(_player.MaxFreq) - Math.Log10(_player.MinFreq)) + Math.Log10(_player.MinFreq));
+        }
+
+        // Convert a logarithmic frequency to a "linear" value (0 - 1)
+        private double LogToLinear(float freq)
+        {
+            return (double) ((Math.Log10(freq) - Math.Log10(_player.MinFreq)) / (Math.Log10(_player.MaxFreq) - Math.Log10(_player.MinFreq)));
         }
     }
 }
